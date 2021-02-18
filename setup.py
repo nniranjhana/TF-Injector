113247c2783fbf79a152dd02b94c842e68edb5bc
Ali Asgari <altostratous@gmail.com>
2021-02-18 12:08:42 -0800

    Add pip packaging

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="TF-Injector",
+    version="0.0.1",
+    description="A pip package to inject faults into TensorFlow v2 models",
+    license="MIT",
+    author="Niranjhana Narayanan",
+    packages=['src'],
+    install_requires=[
+        'PyYAML>=5.4.1',
+        'tensorflow>=2.4.1'
+    ],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
